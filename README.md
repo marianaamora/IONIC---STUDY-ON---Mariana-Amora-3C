6e3206525482aef12a42016d173b428402e30c7a
marianaamora <130800400+marianaamora@users.noreply.github.com>
2023-05-26 19:59:30 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# IONIC---STUDY-ON---Mariana-Amora-3C
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ionic-5-angular-10-start-template-dycfym)
---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss'],
+})
+export class Tab2Page {
+  constructor() {}
+
+  option = {
+    slidesPerView: 1.5,
+    centeredSlides: true,
+    loop: true,
+    spaceBetween: 10,
+  };
+}
